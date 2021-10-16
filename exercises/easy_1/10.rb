06bcd425627cbc9ff4d700adc10beea85cb74423
Stan van de Poll <swvandepoll@gmail.com>
2021-10-16 20:37:03 +0200

    finish easy 1 set

---
+++ exercises/easy_1/10.rb
@@ -0,0 +1,16 @@
+def count(array)
+  counter = 0
+
+  array.map do |element|
+    counter += 1 if yield(element)
+  end
+
+  counter
+end
+
+p count([1,2,3,4,5]) { |value| value.odd? } == 3
+p count([1,2,3,4,5]) { |value| value % 3 == 1 } == 2
+p count([1,2,3,4,5]) { |value| true } == 5
+p count([1,2,3,4,5]) { |value| false } == 0
+p count([]) { |value| value.even? } == 0
+p count(%w(Four score and seven)) { |value| value.size == 5 } == 2
