0cbc5eb10de79649ae6332a43aa0a8604c8b3a24
Stan van de Poll <swvandepoll@gmail.com>
2021-10-17 12:52:11 +0200

    finish easy 2

---
+++ exercises/easy_2/9.rb
@@ -0,0 +1,27 @@
+def each_cons(array)
+  0.upto(array.length - 2).each do |starting_index|
+    yield(array[starting_index, 2])
+  end
+  nil
+end
+
+hash = {}
+result = each_cons([1, 3, 6, 10]) do |value1, value2|
+  hash[value1] = value2
+end
+p result == nil
+p hash == { 1 => 3, 3 => 6, 6 => 10 }
+
+hash = {}
+result = each_cons([]) do |value1, value2|
+  hash[value1] = value2
+end
+p hash == {}
+p result == nil
+
+hash = {}
+result = each_cons(['a', 'b']) do |value1, value2|
+  hash[value1] = value2
+end
+p hash == {'a' => 'b'}
+p result == nil
