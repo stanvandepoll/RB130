d721ecd86fc3b4967ef5bbbbf818e27e827208c2
Stan van de Poll <swvandepoll@gmail.com>
2021-10-25 11:58:01 +0200

    finish up diamond for sharing

--- challenges/medium/diamond.rb
+++ challenges/medium/diamond.rb
@@ -1,20 +1,5 @@
-# The diamond exercise takes as its input a letter, and outputs it
-# in a diamond shape. Given a letter, it prints a diamond starting
-# with 'A', with the supplied letter at the widest point.
-
-# The first row contains one 'A'.
-# The last row contains one 'A'.
-# All rows, except the first and last, have exactly two identical letters.
-# The diamond is horizontally symmetric.
-# The diamond is vertically symmetric.
-# The diamond has a square shape (width equals height).
-# The letters form a diamond shape.
-# The top half has the letters in ascending order.
-# The bottom half has the letters in descending order.
-# The four corners (containing the spaces) are triangles.
-
 # input: letter
-# output: diamond according to rules
+# output: diamond string according to rules
 
 # the letter part goes from A up to given letter, and back to A
 # the amount of spaces on the left goes from n to 0, back to n
