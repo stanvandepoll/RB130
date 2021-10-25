2f48bafcb828a3d32c4b8db4cbdbde43f3e3ed8b
Stan van de Poll <swvandepoll@gmail.com>
2021-10-25 13:40:36 +0200

    solve robot name challenge

---
+++ challenges/medium/robot.rb
@@ -0,0 +1,70 @@
+=begin
+Write a program that manages robot factory settings.
+
+When robots come off the factory floor, they have no name.
+The first time you boot them up, a random name is generated,
+such as RX837 or BC811.
+
+Every once in a while, we need to reset a robot to its factory
+settings, which means that their name gets wiped. The next time
+you ask, it will respond with a new random name.
+
+The names must be random; they should not follow a predictable
+sequence. Random names means there is a risk of collisions.
+Your solution should not allow the use of the same name twice when avoidable.
+
+input:  - name instance method call, no arguments
+        - reset call
+        - name getter
+output: name string, 2 uppercase letters followed by 3 digits
+        random generated but avoid taken names
+
+test cases:
+  derived input and output from test cases
+  
+data structure:
+  string for name
+  ranges for values to pick for random name generation
+  array of taken names (no need to implement cleanup for deleted robots)
+
+algo:
+  generate name upon initialization
+  generate name upon reset
+    name generation:
+      pick letter twice from given range 'A'-'Z'
+      pick digits thrice from range 0-9
+      check against used names, repeat if name already used
+      add generated name to used names (class variable)
+      return generated name
+    end
+=end
+
+class Robot
+  @@used_names = []
+
+  def initialize
+    reset
+  end
+
+  def reset
+    @name = generate_name
+  end
+
+  def name
+    @name
+  end
+
+  private
+
+  def generate_name
+    generated_name = nil
+    loop do
+      letters = ('A'..'Z').to_a
+      digits = ('0'..'9').to_a
+      generated_name = letters.sample + letters.sample + digits.sample + digits.sample + digits.sample
+      break unless @@used_names.include?(generated_name)
+    end
+    @@used_names << generated_name  
+    generated_name
+  end
+end
